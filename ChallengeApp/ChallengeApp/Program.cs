bda635beba2c377fce840c545ece133dc43a60bc
Gerber2525 <gerber2525@gmail.com>
2024-01-24 00:42:28 +0100

    4 lekcja

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,22 @@
+﻿var name = "Kasia";
+var age = 20;
+var sex = "kobieta";
+
+if (sex == "kobieta" && age > 30)
+{
+    Console.WriteLine("Kobieta poniej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+
+}
+
+
