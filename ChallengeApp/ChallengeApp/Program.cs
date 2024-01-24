47ca420ff64c3d921194c2abb2164245edce5504
Gerber2525 <gerber2525@gmail.com>
2024-01-24 23:20:27 +0100

    Dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,68 @@
-﻿var name = "Kasia";
-var age = 20;
-var sex = "kobieta";
+﻿int numer = 3936;
+string slowo = numer.ToString();
+char[] litery = slowo.ToArray();
 
-if (sex == "kobieta" && age < 30)
+int liczba0 = 0;
+int liczba1 = 0;
+int liczba2 = 0;
+int liczba3 = 0;
+int liczba4 = 0;
+int liczba5 = 0;
+int liczba6 = 0;
+int liczba7 = 0;
+int liczba8 = 0;
+int liczba9 = 0;
+
+foreach (char liczba in litery)
 {
-    Console.WriteLine("Kobieta poniej 30 lat");
+    if (liczba == '0')
+    {
+        liczba0++;
+    }
+    else if (liczba == '1')
+    {
+        liczba1++;
+    }
+    else if (liczba == '2')
+    {
+        liczba2++;
+    }
+    else if (liczba == '3')
+    {
+        liczba3++;
+    }
+    else if (liczba == '4')
+    {
+        liczba4++;
+    }
+    else if (liczba == '5')
+    {
+        liczba5++;
+    }
+    else if (liczba == '6')
+    {
+        liczba6++;
+    }
+    else if (liczba == '7')
+    {
+        liczba7++;
+    }
+    else if (liczba == '8')
+    {
+        liczba8++;
+    }
+    else if (liczba == '9')
+    {
+        liczba9++;
+    }
 }
-else if (name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa, lat 30");
-}
-else if (sex == "mężczyzna" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else
-{
-    Console.WriteLine("Inna osoba");
-}
+Console.WriteLine("0=>" + liczba0);
+Console.WriteLine("1=>" + liczba1);
+Console.WriteLine("2=>" + liczba2);
+Console.WriteLine("3=>" + liczba3);
+Console.WriteLine("4=>" + liczba4);
+Console.WriteLine("5=>" + liczba5);
+Console.WriteLine("6=>" + liczba6);
+Console.WriteLine("7=>" + liczba7);
+Console.WriteLine("8=>" + liczba8);
+Console.WriteLine("9=>" + liczba9);
